9e8984b31b9f4d782ad26daf2aaf6ce5691cd6f5
j3susft <cejesusft10@gmail.com>
2026-04-20 12:33:31 -0600

    Add files via upload

---
+++ ENERGIA_CINETICA_FLORES_TORRES_CEDRICK_JESUS.py
@@ -0,0 +1,5 @@
+#Energía cinética Ek=0.5m*v^2
+masa=float(input("Ingrese la masa en kg: "))
+velocidad=float(input("Ingrese la velocidad en m/s: "))
+energía_cinetica=0.5*masa*velocidad**2
+print(f"La energía cinética es: {energía_cinetica} joules")
---
+++ ENERGIA_FLORES_TORRES_CEDRICK_JESUS.py
@@ -0,0 +1,5 @@
+#energia E=mc^2
+masa=float(input("ingrese el valor de la masa en kg: "))
+velocidad_de_la_luz=3e8
+energia=masa*velocidad_de_la_luz*2
+print(f"la energia equivalente es {energia} jolues")
